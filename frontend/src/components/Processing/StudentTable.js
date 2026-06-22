210d0d709d489b5b2f3b969a486135a1a45c7155
NahidJB1 <nahidjahanb1@gmail.com>
2026-06-22 17:24:47 +0800

    Fix missing React import

--- frontend/src/components/Processing/StudentTable.js
+++ frontend/src/components/Processing/StudentTable.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import StatusDropdown from './StatusDropdown';
 import ActionMenu from './ActionMenu';
 
